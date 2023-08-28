eb52039f97ce56b63800b0b9f05291990ae0370f
dhushy123 <dhushy2011@live.co.uk>
2023-08-28 18:04:36 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import quotesData from './quotes.json'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+class App extends Component {
+  constructor(props) {
+    super(props); 
+    this.state={
+      currentQuoteIndex:0,
+    };
+    this.getQuote=this.getQuote.bind(this)
+  }
+
+
+  componentDidMount() {
+    // Code to execute when the component mounts
+    this.getQuote();
+  }
+
+  getQuote(){
+    const {currentQuoteIndex} =this.state;
+    const totalQuotes =quotesData.length;
+    const nextIndex = (currentQuoteIndex + 1) % totalQuotes;
+
+    this.setState({
+      currentQuoteIndex: nextIndex,
+    })
+  }
+
+
+
+  render() {
+
+    const {currentQuoteIndex}=this.state;
+    const currentQuote = quotesData[currentQuoteIndex];
+    return (
+      <div>
+        <div className="quote-box" id="quote-box">
+          <div id="text" >{ currentQuote.text }</div>
+          <div id="author">{currentQuote.author}</div>
+          <button id="new-quote" onClick={this.getQuote}>New Quote</button>
+          <a  href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(currentQuote.text + " - " + currentQuote.author)}`}
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+         id="tweet-quote">tweet</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
